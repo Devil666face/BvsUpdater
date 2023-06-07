4a71499ade207f7d96e0879985364e32ff9bf68e
Devil666face <artem1999k@gmail.com>
2023-06-07 15:55:45 +0300

    fix all

--- .dev/build.sh
+++ .dev/build.sh
@@ -5,8 +5,7 @@
 --follow-imports \
 --include-plugin-directory=./venv/lib/python3.10/site-packages/apscheduler \
 main.py
-# ARCHIVE_NAME="${PWD##*/}.tgz"
-ARCHIVE_NAME="${PWD##*/}.txz"
+ARCHIVE_NAME="${PWD##*/}.tgz"
 tar -cvzf $ARCHIVE_NAME main.bin drw/ ksc/ script/ updater.yaml bvsupdater.service .env.sample init.sh
 # XZ_OPT=-e9T0 tar cJf $ARCHIVE_NAME main.bin drw/ ksc/ script/ updater.yaml bvsupdater.service .env.sample init.sh
 mkdir -p ./dist && mv $ARCHIVE_NAME ./dist
